b6e32a36c2f8ba57ca330c2697951d130cccd2cd
shivaram-raut <shivaramraut02@gmail.com>
2024-07-02 12:28:57 +0545

    Another update: solves the redundant folder creation issue.

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import shutil
 
 
 # function to move files to the respective folders:
-def move(file_type, folder_name):
-    source_path = os.path.join(path, file)
-    destination_path = os.path.join(path, folder_name, file)
+def move(file_type, file_name,  folder_name):
+    # creating folder if it doesn't already exist.
+    new_folder = os.path.join(path, folder_name)  # path => path provided by user
+    if not os.path.exists(new_folder):
+        os.mkdir(new_folder)
+
+    source_path = os.path.join(path, file_name)
+    destination_path = os.path.join(path, folder_name, file_name)
     if not os.path.exists(destination_path):
         try:
             shutil.move(source_path, destination_path)
@@ -19,39 +24,33 @@ def move(file_type, folder_name):
 
 
 # specifying the name of the directory to be sorted:
-path = "C:\\Users\\Acer\\Documents_copy\\"
+# path = "C:\\Users\\Acer\\Desktop\\Questions\\"
+path = input("Enter the path ::")
 
 # making  list of files and folders which are inside  the directory pointed by the path.
 contents = os.listdir(path)
 
-
-# creating new folders only if they don't already exist:
-folders_name_list = ['images', 'Notepad(text)', '.exe files', 'pdf', 'ms-word', 'html']
-for folder in folders_name_list:
-    new_folder = os.path.join(path, folder)
-    if not os.path.exists(new_folder):
-        os.mkdir(new_folder)
-
-
 for file in contents:
     # moving text files to Notepad(text)
     if ".txt" in file:
-        move(".txt", "Notepad(text)")
+        move(".txt", file, "Notepad(text)")
 
     # moving .exe files to '.exe files'
-    elif ".exe" in file:
-        move(".exe", ".exe files")
+    if ".exe" in file:
+        move(".exe", file, ".exe files")
 
     # moving .html files to html
-    elif ".html" in file:
-        move(".html","html")
+    if ".html" in file:
+        move(".html", file, "html")
 
     # moving .pdf files to pdf:
-    elif ".pdf" in file:
-        move(".txt", "pdf")
+    if ".pdf" in file:
+        move(".pdf", file, "pdf")
 
     # moving .png files to images:
-    elif ".png" in file:
-        move(".png", "images")
-
+    if ".png" in file:
+        move(".png", file, "images")
+    # moving .docx files to 'Ms-word files'
+    if ".docx" in file:
+        move(".docx", file,"Ms-word files")
 
